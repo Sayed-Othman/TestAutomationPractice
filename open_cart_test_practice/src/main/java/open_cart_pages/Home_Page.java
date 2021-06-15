88850d41ef50ed17df87182ef9e89a4d63f9c04e
Sayed Othman <sayed.othman137@gmail.com>
2021-06-15 22:31:51 +0200

    TestAutomationProject

---
+++ open_cart_test_practice/src/main/java/open_cart_pages/Home_Page.java
@@ -0,0 +1,41 @@
+package open_cart_pages;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+
+import com.shaft.gui.element.ElementActions;
+
+public class Home_Page {
+	
+	private WebDriver  driver ; 
+	
+	
+	
+	 // Constructor
+	
+	public Home_Page (WebDriver driver) {
+		
+		this.driver = driver;
+	}
+	
+	
+	// Elements Locators 
+	
+	private By MyAccount_Button = By.xpath("//a[contains(.,'My Account')and @class='dropdown-toggle']");
+	private By Register_Button 	= By.xpath("//a[contains(.,'Register')]");
+	
+	
+	//Actions 
+	
+	
+	public void  NavigateToRegisterationPage () {
+		
+		ElementActions.click(driver, MyAccount_Button);
+		ElementActions.click(driver, Register_Button );
+		
+	}
+	
+
+	
+
+}
